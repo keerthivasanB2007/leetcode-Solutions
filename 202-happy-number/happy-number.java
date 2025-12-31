fc3ff94ef990f995a6650fa78e630e6396848136
keerthivasan B <keerthivasanbn@gmail.com>
2025-12-31 19:04:58 +0530

    Time: 1 ms (78.97%) | Memory: 42 MB (96.59%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+         List<Integer> r=new ArrayList<>();
+     int sum=0;
+          while(sum!=1){
+           sum=0;
+            while(n>0){
+              sum=sum+ (n%10)*(n%10);
+              n=n/10;
+            }
+            n=sum;
+            if(r.contains(sum)){
+                return false;
+            }
+            r.add(sum);
+           
+          }
+          return true;
+    }
+}
