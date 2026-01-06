6ef5664bc3cb622d2a95680198e16f9381acfad0
keerthivasan B <keerthivasanbn@gmail.com>
2026-01-06 17:47:29 +0530

    Time: 3 ms (63.97%) | Memory: 46.8 MB (15.18%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String r="";
+      /*  String r="";
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
             if(((c>='a' && c<='z') || (c<='Z' && c>='A')) || (c>='0' && c<='9')){
@@ -18,6 +18,31 @@ class Solution {
          i++;
          j--;
        }
-        return true;
+        return true;*/
+         int i=0;
+         int j=s.length()-1;
+        
+       while(i<j){
+         boolean notchange=true;
+         char c=s.charAt(i);
+         char x=s.charAt(j);
+        if(!(((c>='a' && c<='z') || (c<='Z' && c>='A')) || (c>='0' && c<='9'))){
+               i++;
+               notchange=false;
+        }
+        if(!(((x>='a' && x<='z') || (x<='Z' && x>='A')) || (x>='0' && x<='9'))){
+            j--;
+            notchange=false;
+        }
+
+        if( notchange && !(Character.toString(x).equalsIgnoreCase(Character.toString(c)))){
+            return false;
+        }
+        if(notchange){
+            i++;
+            j--;
+        }
+       }
+       return true;
+        }
     }
-}
