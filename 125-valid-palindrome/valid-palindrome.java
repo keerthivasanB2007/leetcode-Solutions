3fc9376b82acf7c4408105ce3292842bb9f59121
keerthivasan B <keerthivasanbn@gmail.com>
2026-01-06 17:57:21 +0530

    Time: 2 ms (98.83%) | Memory: 44.2 MB (85.80%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,40 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-      /*  String r="";
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(((c>='a' && c<='z') || (c<='Z' && c>='A')) || (c>='0' && c<='9')){
-                
-                c=Character.toLowerCase(c);
-               
-                r=r+c;
-            }
-        }
-      
-       int i=0,j=r.length()-1;
-       while(i<j){
-        if(r.charAt(i)!=r.charAt(j))
-            return false;
-         i++;
-         j--;
-       }
-        return true;*/
+    
          int i=0;
          int j=s.length()-1;
         
        while(i<j){
-        // boolean notchange=true;
          char c=s.charAt(i);
          char x=s.charAt(j);
         if(!(((c>='a' && c<='z') || (c<='Z' && c>='A')) || (c>='0' && c<='9'))){
                i++;
                continue;
-              // notchange=false;
+           
         }
         if(!(((x>='a' && x<='z') || (x<='Z' && x>='A')) || (x>='0' && x<='9'))){
             j--;
             continue;
-          //  notchange=false;
         }
 
         if(  (Character.toLowerCase(x)!=Character.toLowerCase(c))){
