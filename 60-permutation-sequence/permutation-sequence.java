33580bae3ae6822544d9c4ba46edc410bdc48603
keerthivasan B <keerthivasanbn@gmail.com>
2026-01-27 22:24:52 +0530

    Time: 1214 ms (5.49%) | Memory: 47.2 MB (6.00%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.java
+++ 60-permutation-sequence/permutation-sequence.java
@@ -1,27 +1,23 @@
-class Solution {
-    
-    
+class Solution {  
   int q=0;
   String r="";
   int m=1;
    void per(int[] i,String l){
-        if(m>q)
-          return;
+        
         if(l.length() == (i.length-1)  )
         {
-          
             if(m==q)
                r=l;
             m++;
             return;
         }
-        
-
         for(int j=0;j<=i.length+1;j++){
             if((j+1)<i.length && i[j+1]!=1){
                 int []c=Arrays.copyOf(i,i.length);
                 c[j+1]=1;
-                String b=l+(j+1);      
+                String b=l+(j+1); 
+                if(m>q)
+                   break;
                 per(c,b);
             }
         }
@@ -30,10 +26,7 @@ class Solution {
       String l=new String();
       l="";
       q=k;
-      
         int []i=new int[n+1];
-        
-        
         per(i,l);
         return r;
     }
