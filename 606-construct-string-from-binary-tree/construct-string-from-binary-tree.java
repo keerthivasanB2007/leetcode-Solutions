aae0650d3292ff341d3ee316d4a7394c7b740850
keerthivasan B <keerthivasanbn@gmail.com>
2026-07-28 18:06:04 +0530

    Time: 2 ms (86.66%) | Memory: 46.4 MB (68.51%) - LeetSync

--- 606-construct-string-from-binary-tree/construct-string-from-binary-tree.java
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.java
@@ -14,25 +14,44 @@
  * }
  */
 class Solution {
-    public String printString(TreeNode root){
-            if(root==null)
-               return "";
+    // public String printString(TreeNode root){
+    //         if(root==null)
+    //            return "";
             
-            String left = printString(root.left);
-            String right = printString(root.right);
+    //         String left = printString(root.left);
+    //         String right = printString(root.right);
             
-            if(left.length()==0 && right.length()==0)
-                return root.val+"";
-            else if(left.length()==0 && right.length()!=0)
-                return root.val + "()(" + right + ")";
-            else if(left.length()!=0 && right.length()!=0)
-                return root.val + "(" + left+ ")" + "(" + right + ")";
-            else
-                return root.val +"(" +left +")";
+    //         if(left.length()==0 && right.length()==0)
+    //             return root.val+"";
+    //         else if(left.length()==0 && right.length()!=0)
+    //             return root.val + "()(" + right + ")";
+    //         else if(left.length()!=0 && right.length()!=0)
+    //             return root.val + "(" + left+ ")" + "(" + right + ")";
+    //         else
+    //             return root.val +"(" +left +")";
         
+    // }
+    public void printString(TreeNode root, StringBuilder a){
+        if(root==null)
+          return;
+        
+        a.append(root.val);
+
+        if(root.left!=null || root.right!=null){
+            a.append("(");
+            printString(root.left,a);
+            a.append(")");
+        }
+        if(root.right!=null){
+            a.append("(");
+            printString(root.right,a);
+            a.append(")");
+        }
     }
     public String tree2str(TreeNode root) {
-        String result = printString(root);
-        return result;
+        StringBuilder a = new StringBuilder();
+        printString(root,a);
+        
+        return a.toString();
     }
 }
