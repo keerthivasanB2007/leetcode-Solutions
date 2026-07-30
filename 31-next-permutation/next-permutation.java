97fc178d6d2403fd4bd3841b6c03b778cb1b6936
keerthivasan B <keerthivasanbn@gmail.com>
2026-07-30 18:32:38 +0530

    Time: 2 ms (7.24%) | Memory: 44.7 MB (32.85%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -10,7 +10,7 @@ class Solution {
                     break;
             }
         }
-        System.out.println("pivot: "+nums[pivot] +f);
+      
         if(!f){
             int i = 0;
             int j = nums.length-1;
@@ -27,22 +27,22 @@ class Solution {
            return;
         }
         int x=0;
-        System.out.println(Arrays.toString(nums));
+     
         for(int i = nums.length-1; i>=0;i--){
             if(nums[pivot] < nums[i]){
-                System.out.println(nums[i]);
+      
                 int t = nums[pivot];
                 nums[pivot]=nums[i];
                 nums[i]=t;
                 x=i;
-                System.out.println(Arrays.toString(nums) + " i ="+i +" pivot ="+pivot);
+               
                 break;
             }
         }
-         int i = x;
+     
             int j = nums.length;
             Arrays.sort(nums,pivot+1,j);
-            System.out.println(Arrays.toString(nums));
+         
 
         
  }
