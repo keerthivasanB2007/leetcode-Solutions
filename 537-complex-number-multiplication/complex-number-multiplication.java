9b788c6beb88563f2d3c0aea23f768fbd3dc2002
keerthivasan B <keerthivasanbn@gmail.com>
2026-08-01 21:04:09 +0530

    Time: 7 ms (8.10%) | Memory: 43.2 MB (8.10%) - LeetSync

---
+++ 537-complex-number-multiplication/complex-number-multiplication.java
@@ -0,0 +1,66 @@
+class Solution {
+    int[] midSign = new int[2];
+    int x=0;
+    int getReal(String n){
+        int sign = 1;
+        int i=0;
+        if(n.charAt(0)=='+'){
+          sign=+1;
+            i++;
+        }
+        if(n.charAt(0)=='-'){
+          sign=-1;
+          i++;
+        }
+        
+        while(i<n.length()){
+            if(n.charAt(i)=='+')
+                break;
+            i++;
+
+        }
+       // System.out.println("sign = "+sign);
+        midSign[x++]=i;
+        String s = n.substring(0,i);
+        int r = Integer.parseInt(s);
+        System.out.println("r="+r + "  sign = "+sign);
+        return r;
+    }
+    int getImg(String n)
+    {
+
+         int c = midSign[--x] + 1;
+         int sign = 1;
+         if(n.charAt(c)=='-'){
+            sign = -1;
+            c++;
+         }
+         int start =c;
+         while(c<n.length()){
+            if(n.charAt(c)=='i')
+            {
+                break;
+            }
+            c++;
+         }
+         String s =n.substring(start,c);
+         int r = Integer.parseInt(s);
+         return r*sign;
+    }
+    public String complexNumberMultiply(String num1, String num2) {
+            int a = getReal(num1);   
+            int x = getReal(num2);
+            System.out.println("a="+a);
+            System.out.println("x="+(x));
+
+             int y = getImg(num2);
+            int b = getImg(num1);
+            System.out.println("b="+b);
+            System.out.println("y="+y);
+           
+            int real = (a*x - b*y);
+            int img = (a*y + b*x);
+
+            return real + "+" + img + "i" ;
+    }
+}
