1b0f2c3b7518cacb440b534039780ecd264cf542
keerthivasan B <keerthivasanbn@gmail.com>
2026-08-02 09:48:38 +0530

    Time: 10 ms (10.85%) | Memory: 50 MB (5.29%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,21 @@
+class Solution {
+    Integer[][] dp;
+    public int solve(int l, int r, int[] piles){
+
+            if(l>r)
+               return 0;
+            if(dp[l][r]!=null)
+               return dp[l][r];
+            int left = piles[l] - solve(l+1,r,piles);
+            int right = piles[r] - solve(l,r-1,piles);
+            
+            return dp[l][r]=Math.max(left,right);
+    }
+    public boolean stoneGame(int[] piles) {
+        int n =  piles.length;
+        dp = new Integer[n][n];
+        int r = solve(0,n-1,piles);
+        
+        return r>0?true:false;
+    }
+}
