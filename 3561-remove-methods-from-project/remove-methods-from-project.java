f523ce5391acd6a69cf6408e8c369a2c535eaf73
keerthivasan B <keerthivasanbn@gmail.com>
2026-08-05 18:52:55 +0530

    Time: 269 ms (5.77%) | Memory: 283.3 MB (61.54%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -0,0 +1,79 @@
+class Solution {
+    Set<Integer> bug = new HashSet<>();
+    public void dfs(HashMap<Integer,List<Integer>> graph,Set<Integer> visited,int k){
+            Stack<Integer> s = new Stack<>();
+            s.push(k);
+           
+            while(!s.isEmpty()){
+                int currentNode = s.pop();
+                if(!visited.contains(currentNode)){
+                    visited.add(currentNode);
+                     bug.add(currentNode);
+
+                    List<Integer> neighbors = graph.getOrDefault(currentNode,new ArrayList<>());
+                    for(int i=0;i<neighbors.size();i++){
+                         int neighbor = neighbors.get(i);
+                       if(!visited.contains(neighbor)){
+    
+                            s.push(neighbor);
+                        }
+                    }
+                }
+
+            }
+    }
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+            HashMap<Integer,List<Integer>> graph = new HashMap<>();
+            for(int i=0;i<n;i++){
+                    List<Integer> t = new ArrayList<>();
+                    graph.put(i,t);
+            }
+
+            for(int i=0;i<invocations.length;i++){
+                    int key = invocations[i][0];
+                  List<Integer> l =graph.get(key);
+                  l.add(invocations[i][1]);
+                  graph.put(key,l);
+            }
+            // System.out.println(graph);
+          //  boolean[] visited = new boolean[n];
+            Set<Integer> visited = new HashSet<>();
+            dfs(graph,visited,k);
+            if(visited.size()==n)
+                return new ArrayList<>();
+            // System.out.println(Arrays.toString(visited));
+            List<Integer> result = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                if(bug.contains(i))
+                  continue;
+                List<Integer> neighbors = graph.getOrDefault(i,new ArrayList<>());
+                boolean touch = false;
+                for(int j=0;j<neighbors.size();j++){
+                        if(bug.contains(neighbors.get(j))){
+                            touch=true;
+                            break;
+                        }
+                }
+                if(touch){
+                    List<Integer> result1 = new ArrayList<>();
+                     for(int x=0;x<n;x++)
+                         result1.add(x);
+                        return result1;
+                    
+                   // result.add(i);
+                }
+                else{
+                    result.add(i);
+                }
+
+            }
+            System.out.println("visited : "+visited);
+        
+        // if(result.size()==0){
+           
+        // }
+           
+            return result;
+
+    }
+}
