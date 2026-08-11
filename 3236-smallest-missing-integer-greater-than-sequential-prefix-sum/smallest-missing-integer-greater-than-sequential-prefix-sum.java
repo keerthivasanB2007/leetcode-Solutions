f252da07f5b0f14570da6fdaa25983cda9e3fbf6
keerthivasan B <keerthivasanbn@gmail.com>
2026-08-11 21:04:03 +0530

    Time: 4 ms (19.94%) | Memory: 44.4 MB (23.29%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -5,24 +5,14 @@ class Solution {
         int sum = 0;
         
         int j=1;
-        int max =0;
         while(j<nums.length){
-        //    System.out.println("nums[i]="+nums[i]);
-       
-           
-          
             if((nums[j-1]+1)== nums[j]){
-                sum = sum + nums[j-1];
-               
+                sum = sum + nums[j-1];    
             }
             else
              {
-                // if((j-1)==0  || (nums[j-1] == (nums[j-2]+1)))
-                //     sum = sum + nums[j-1];
-             
                 break;
              }
-          
           j++;
         }
    
