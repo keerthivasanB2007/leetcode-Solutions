43049551cb0e48fa4b9ab67ef4ef77d1a640bc98
keerthivasan B <keerthivasanbn@gmail.com>
2026-08-15 10:20:52 +0530

    Time: 1 ms (100.00%) | Memory: 133.4 MB (75.00%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
@@ -3,7 +3,7 @@ class Solution {
         if((nums.length == 2) && (nums[0]^nums[1])==0){
                 return 1;
         }
-                System.out.println(nums.length);
+   
         int x = nums[0];
         int z=0;
         for(int i=1;i<nums.length;i++){
@@ -12,42 +12,13 @@ class Solution {
               z++;
            
         }
-        System.out.println("z="+z);
+      
         if(x!=0)
            return nums.length;
         if(z==(nums.length-1))
           return 0;
-        if(x==0)
+      
           return nums.length-1;
-          System.out.println(nums.length);
-        List<Integer> s = new ArrayList<>();
-        int i=1;
-        int xor = nums[0];
-        s.add(xor);
-        int max=0;
-        while(i<nums.length){
         
-                int temp = (xor ^ nums[i]);
-             
-                if((temp)!=0){
-                    s.add(nums[i]);
-               
-                    xor=temp;
-                    
-                    i++;
-                }
-                else{
-                    s.clear();
-               
-                    s.add(nums[i]);
-                    xor=nums[i];
-                    i++;
-              
-                }
-                max= Math.max(s.size(),max);
-            
-         }
-
-           return max;
     }
 }
